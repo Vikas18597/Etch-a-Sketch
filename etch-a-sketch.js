5d02a7bf66480fbc0a100074bc1a56775b2bc7d8
Vikas <khativikas97@gmail.com>
2023-02-19 15:31:56 +0530

    Completed 6*6 grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -6,17 +6,34 @@ document.body.appendChild(element);
 // changing properties of parent div element
 
 document.getElementById('parent_div').style.display = 'flex';
-document.getElementById('parent_div').style.minWidth = '100%' ;
-document.getElementById('parent_div').style.minHeight = '100%' ;
+document.getElementById('parent_div').style.width = '100%' ;
+document.getElementById('parent_div').style.height = '100vh' ;
 document.getElementById('parent_div').style.justifyContent = 'space-between' ;
 
 // creating multiple divs inside the parent div 
-let div_number = 1;
+
 for (i = 0; i < 6 ; i++){    
     let element = document.createElement("div") ;
-    element.setAttribute('class', "1st_lvl_div" + div_number); 
-    let text = document.createTextNode("Child div no. " + div_number);
-    element.appendChild(text);
+    element.setAttribute('id', "1st_lvl_div" + i); 
     document.getElementById('parent_div').appendChild(element);
-    div_number ++ ; 
+    document.getElementById('1st_lvl_div' + i).style.display = 'flex';
+    document.getElementById('1st_lvl_div' + i).style.flexDirection = 'column';    
+    document.getElementById('1st_lvl_div' + i).style.justifyContent = 'space-between' ;
+    
+    for(j = 0; j <6 ; j ++){
+        let element = document.createElement("div") ;
+        element.setAttribute('id', "2nd_lvl_div" + j + i); 
+        let text = document.createTextNode("Child div no. ( " + j + ", " + i +" )");
+        element.appendChild(text);
+        document.getElementById('1st_lvl_div' + i).appendChild(element);
+        document.getElementById('2nd_lvl_div' + j + i).style.border = 'solid' ; 
+    }
+    
 }
+
+let x = document.getElementById('.2nd_lvl_div0')
+console.log(x)
+
+
+
+
